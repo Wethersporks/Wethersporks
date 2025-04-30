1d65f3d60e250cd9e2a409164804be6e24e8894e
Patrick <patrickwigley2@gmail.com>
2025-04-30 18:58:25 +0100

    Added Reservation Part (DB)

--- WetherSporks/ReservationBooking_WSApp/admin.py
+++ WetherSporks/ReservationBooking_WSApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+
+admin.site.register(Tables)
+admin.site.register(TimeSlots)
+admin.site.register(Customers)
+admin.site.register(ResStatuses)
+admin.site.register(Reservations)
--- WetherSporks/ReservationBooking_WSApp/models.py
+++ WetherSporks/ReservationBooking_WSApp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+#PREFIXING ALL MODELS WITH M_
+
+class Tables(models.Model):
+    table_no = models.IntegerField()
+    seat_count = models.IntegerField()
+
+class TimeSlots(models.Model):      # TODO: Make factory for
+    start_date = models.DateField()
+    start_time = models.TimeField()
+    duration = models.DurationField(null=False)
+    table = models.ForeignKey(Tables, on_delete=models.CASCADE) #FK
+
+
+class Customers(models.Model):
+    name = models.CharField(max_length=30)
+    number = models.CharField(max_length=11)
+    email = models.CharField(max_length=50)
+
+class ResStatuses(models.Model):
+    status = models.CharField(max_length=9)
+
+
+class Reservations(models.Model):    
+    guest_count = models.IntegerField(default=1)
+    customer = models.ForeignKey(Customers, on_delete=models.CASCADE) # FK
+    start_time = models.ForeignKey(TimeSlots, on_delete=models.CASCADE) # FK
+    status = models.ForeignKey(ResStatuses, on_delete=models.CASCADE) #FK
+
+
+
+
