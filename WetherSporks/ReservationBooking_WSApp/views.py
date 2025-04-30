71bfd2f5e54432dbb6aae05b0e395efcf6f04cd7
Patrick <patrickwigley2@gmail.com>
2025-04-30 17:19:49 +0100

    Fitted new front-end code into django

--- WetherSporks/ReservationBooking_WSApp/views.py
+++ WetherSporks/ReservationBooking_WSApp/views.py
@@ -5,4 +5,18 @@ from django.shortcuts import render
 def index(request):
     """ Home-Page """
     
+    if "time" in request.GET:
+        time_selected = request.GET["time"]
+        quantity = request.GET["guestCount"]
+        date = request.GET["date"]
+
+        print(f"Time Picked: {time_selected} for {quantity} people on {date}")
+
+
+    elif "date" in request.GET:
+        date_selected = request.GET["date"]
+        return render(request, "ReservationBooking/Index.html", {"date_selected":date_selected})
+
+
+
     return render(request, "ReservationBooking/Index.html")
